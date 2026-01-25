76f40915a75164d4eaff3b6644dfc2b38fc30f2b
Pablo J. Martínez <pjmm93@gmail.com>
2026-01-25 21:49:31 +0100

    chore: reorganize the repository into a domain-driven structure (src, tests, legacy) and standardize project naming

---
+++ legacy/PabloJMartinez.AStar/Navmesh Editor/NavmeshEditor.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2013-2017 Pablo J. Martínez. All rights reserved.
+// Licensed under the MIT License.
+// Part of the legacy PabloJMartinez.AStar implementation.
+
+using UnityEngine;
+using System.Collections;
+
+namespace ComingLights
+{
+    public class NavmeshEditor
+    {
+        public static bool isActive = true;
+    }
+}
